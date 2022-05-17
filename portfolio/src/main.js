49346ddcfbb41990df9acdc4b0d49f8c8116acfc
P2-openclassroom <jonh_06@live.fr>
2022-05-17 10:48:54 +0200

    add font mdi

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/css/index.css'
+// via npm install @mdi/font
+import "@mdi/font/css/materialdesignicons.css";
 
 const app = createApp(App)
 .use(router)
