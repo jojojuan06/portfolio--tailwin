cc17572f5b6ac8e956fa22ee2771e6c6e06d5883
P2-openclassroom <jonh_06@live.fr>
2022-05-17 10:49:51 +0200

    change url img

--- portfolio/src/components/CardList.vue
+++ portfolio/src/components/CardList.vue
@@ -20,49 +20,49 @@ components: {
                 {
                     projectnumber:"01",
                     name: 'Prenez en main votre formation de dévloppeur web',
-                    image: '../images/project01.png',
+                    image: '/src/assets/images/project01.png',
                     descriptions: 'Mettre en place un systeme de veille,Développez vos soft skills,Découvrez les métiers de développeur',
                     duration: '20 heures'
                 },
                 {
                     projectnumber:"02",
                     name: 'Transformez une maquette en site web avec HTML & CSS' ,
-                    image: '../images/project02.png',
+                    image: '/src/assets/images/project02.png',
                     descriptions: 'créer ce prototype en intégrant la maquette en HTML et CSS (utilisation de grid et flexbox)',
                     duration: '90 heures'
                 },
                 {
                     projectnumber:"03",
                     name: 'Dynamisez une page web avec des animations CSS' ,
-                    image: '../images/project03.png',
+                    image: '/src/assets/images/project03.png',
                     descriptions: 'créer des animations css modernes,utilisation de Git/Github,utiliser sass',
                     duration: '70 heures'
                 },
                 {
                     projectnumber:"04",
                     name: 'Optimisez un site web existant' ,
-                    image: '../images/project04.png',
+                    image: '/src/assets/images/project04.png',
                     descriptions: 'Amélioration du SEO, de la vitesse de chargement du site, de l’accessibilité',
                     duration: '30 heures'
                 },
                 {
                     projectnumber:"05",
                     name: 'Construisez un site e-commerce en JavaScript' ,
-                    image: '../images/project05.png',
+                    image: '/src/assets/images/project05.png',
                     descriptions: 'Communiquer avec une api en utilisant des methodes https comme fetch et interagissez avec le dom',
                     duration: '90 heures'
                 },
                 {
                     projectnumber:"06",
                     name: 'Construisez une API sécurisée pour une application d\'avis gastronomiques' ,
-                    image: '../images/project06.png',
+                    image: '/src/assets/images/project06.png',
                     descriptions: 'Utilisations de node.js pour le back-end , Utilisation d\'une base de donnée nosql (MongoDb),crée une Api rest (CRUD)',
                     duration: '50 heures'
                 },
                 {
                     projectnumber:"07",
                     name: 'Créez un réseau social d’entreprise' ,
-                    image: '../images/project07.png',
+                    image: '/src/assets/images/project07.png',
                     descriptions: 'En plus d\'un back end avec sequelize/mysql/wamp utiliser un framework-js front-end Vue-js et Vuetify et axios pour les requetes http',
                     duration: '50 heures'
                 },
