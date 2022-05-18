d902e52b644de7b4c1cee06785745dd5189e8ea4
P2-openclassroom <jonh_06@live.fr>
2022-05-18 17:34:22 +0200

    add keyfram animation

--- portfolio/src/views/ContactView.vue
+++ portfolio/src/views/ContactView.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="height: 100vh;"  class="flex justify-center items-center">
         <div class="flex flex-col md:flex-row flex-wrap w-screen justify-center items-center">
-            <div class="barre relative md:mr-12 w-screen text-center" style="max-width:300px">
+            <div class="border--left relative md:mr-12 w-screen text-center" style="max-width:300px">
                 <h2 class="font-bold text-[2rem] pb-4">Let's talk business</h2>
                 <p class="max-w-[300px] text-center">een the industry's standard dummy text ever since the 1500s, 
                     when an unknown printer took a galley of type and scrambled 
@@ -45,7 +45,7 @@
                     </div>
                 </form>
             </div>
-            <div class="barre2 relative"></div>
+            <div class="border--bottom relative"></div>
         </div>
     </div>
 </template>
@@ -66,25 +66,5 @@ export default {
 </script>
 
 <style scoped>
-@media screen and (min-width: 768px){
-    .barre:after{
-    content: "";
-    position: absolute;
-    background: black;
-    width: 4px;
-    height: 400px;
-    left: -40px;
-    bottom: -90px;
-}
-.barre2:before {
-    content: "";
-    position: absolute;
-    background: black;
-    width: 400px;
-    height: 4px;
-    top: 250px;
-    right:1px ;
-    }
-}
-
+@import url('../assets/css/media.css');
 </style>
