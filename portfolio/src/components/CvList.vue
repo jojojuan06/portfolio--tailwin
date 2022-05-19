b99dc8ebfeea101d5e9f191b270166a133626437
P2-openclassroom <jonh_06@live.fr>
2022-05-19 10:59:15 +0200

    add list components and css

---
+++ portfolio/src/components/CvList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="flex ">
+        <ul class="flex flex-wrap">
+            <li style="max-width:400px" class="flex  pb-8 flex-col justify-center items-center" v-for="(cv, index) in listCv" :key="index">
+            <h2 class="text-center">{{cv.title}}</h2>
+            <p class="w-2/4 text-center">{{cv.descriptions}}</p>
+            </li> 
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+name: "CvList",
+    data() {
+        return {
+            listCv: [
+                {
+                    title:"profil",
+                    descriptions: 'Mettre en place un systeme de veille,Développez vos soft skills,Découvrez les métiers de développeur',
+                },
+                {
+                    title:"Formation",
+                    descriptions: 'Mettre en place un systeme de veille,Développez vos soft skills,Découvrez les métiers de développeur',
+                },
+                {
+                    title:"Compétence",
+                    descriptions: 'Mettre en place un systeme de veille,Développez vos soft skills,Découvrez les métiers de développeur',
+                },
+            ]
+        }
+    }
+}
+</script>
---
+++ portfolio/src/views/MonCvView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div style="height: 100vh;" class="flex flex-col justify-start items-center">
+        <div class="flex justify-center items-center relative img--after" >
+            <img src="../assets/images/img--cv.png" class="w-[8rem] " alt="">
+        </div>
+        <CvList/>
+    </div>
+</template>
+
+<script>
+import CvList from "../components/CvList.vue";
+export default {
+    components: { CvList }
+}
+</script>
+
+<style scoped>
+.img--after:before {
+    content:"";
+    position: absolute;
+    background: black;
+    width: 300px;
+    height: 2px;
+    left: 160px;
+    top: 85px;
+    /* animation: bottom-border ease-in-out .9s forwards; */
+}
+.img--after:after {
+    content:"";
+    position: absolute;
+    background: black;
+    width: 300px;
+    height: 2px;
+    right: 160px;
+    top: 85px;
+    /* animation: bottom-border ease-in-out .9s forwards; */
+}
+@keyframes bottom-border {
+    0% { width: 0px; }
+    100% { width: 400px; }
+}
+</style>
