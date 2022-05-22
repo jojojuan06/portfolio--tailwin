1175c0e123f58212466b615301ae1c3475ddd2e4
P2-openclassroom <jonh_06@live.fr>
2022-05-22 10:08:44 +0200

    change css and tailwind

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   content: [ "./index.html",
   "./src/**/*.{vue,js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+        boxShadow:{
+          card:'5px 5px 15px 5px rgba(0,0,0,0.5)'
+      }
+    },
   },
   plugins: [],
 }
